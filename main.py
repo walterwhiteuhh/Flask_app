7b1442432bf145881d0c0b78fef212c2ba747131
Timo <timo.fischer@protonmail.com>
2022-11-15 18:40:08 +0100

    Add basic files

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+app.run(host='0.0.0.0', port=81)
